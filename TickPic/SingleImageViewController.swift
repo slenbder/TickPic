1de751b4f50142bb55e325a17248c3437b5a3297
slenbder <marjassler@ya.ru>
2024-04-21 16:46:49 +0300

    Проведена работа по теме 2/6: Навигация → Уроку 4/7

---
+++ TickPic/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  TickPic
+//
+//  Created by Кирилл Марьясов on 21.04.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
