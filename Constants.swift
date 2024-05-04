397ec204cd4135b58b639d0af6fd2608e41a4324
slenbder <marjassler@ya.ru>
2024-05-04 19:31:23 +0300

    Проведена работа по 2 теме: добавлен файл Constants, в котором создано перечисление Constants, содержащее accessKey, secretKey, redirectURI, accessScope и defaultBaseURL, с присвоенными им значениями

---
+++ Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  TickPic
+//
+//  Created by Кирилл Марьясов on 04.05.2024.
+//
+
+import Foundation
+
+enum Constants {
+    
+    static let accessKey = "QvyZhO6TX0a3DXLiN82ecHIR6SAsCb3EkdBzna6efGM"
+    static let secretKey = "RtcVfP5lt6DNpTlSsFBglUeXNxIAGIwMhx34NVxPCVs"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    
+}
