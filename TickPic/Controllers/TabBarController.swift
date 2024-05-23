e1b6d75f7b5cc3d74748e7791250631f5b3db3dc
slenbder <marjassler@ya.ru>
2024-05-23 18:46:40 +0300

    Создали View Controller из кода!

--- TickPic/Controllers/TabBarController.swift
+++ TickPic/Controllers/TabBarController.swift
@@ -12,15 +12,13 @@ final class TabBarController: UITabBarController {
     override func awakeFromNib() {
         super.awakeFromNib()
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
-        
-        let imagesListViewController = storyboard.instantiateViewController(
-            withIdentifier: "ImagesListViewController"
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil
         )
-        
-        let profileViewController = storyboard.instantiateViewController(
-            withIdentifier: "ProfileViewController"
-        )
-        
         self.viewControllers = [imagesListViewController, profileViewController]
     }
 }
