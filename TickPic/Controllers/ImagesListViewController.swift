3612163ef1f1a160487379c616a051d09ee673ea
slenbder <152342422+slenbder@users.noreply.github.com>
2024-06-03 16:51:36 +0300

    no message

--- TickPic/Controllers/ImagesListViewController.swift
+++ TickPic/Controllers/ImagesListViewController.swift
@@ -1,52 +1,54 @@
 import UIKit
 import Kingfisher
 
-class ImagesListViewController: UIViewController {
+// MARK: - ImagesListViewController
+
+final class ImagesListViewController: UIViewController {
+
     // MARK: - Properties
-    private let showSingleImageSegueIdentifier = "ShowSingleImage"
-    private let imagesListService = ImagesListService.shared
-    private var photos: [Photo] = []
     
-    // MARK: - Outlets
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     @IBOutlet private var tableView: UITableView!
     
-    // MARK: - Lifecycle
+    private var photos: [Photo] = []
+    private let imagesListService = ImagesListService.shared
+    private var imagesListServiceObserver: NSObjectProtocol?
+    
+    // MARK: - View Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupTableView()
+        setupObservers()
+        imagesListService.fetchPhotosNextPage()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-        
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(didReceivePhotos),
-            name: ImagesListService.didChangeNotification,
-            object: nil
-        )
-        
-        imagesListService.fetchPhotosNextPage()
     }
     
-    @objc private func didReceivePhotos() {
-        updateTableViewAnimated()
-    }
-    
-    // MARK: - Private Methods
-    private func updateTableViewAnimated() {
-        let newPhotos = imagesListService.photos
-        let oldCount = photos.count
-        let newCount = newPhotos.count
-        
-        photos = newPhotos
-        
-        if oldCount == 0 {
-            tableView.reloadData()
-        } else {
-            let newIndexPaths = (oldCount..<newCount).map { IndexPath(row: $0, section: 0) }
-            tableView.insertRows(at: newIndexPaths, with: .automatic)
+    private func setupObservers() {
+        imagesListServiceObserver = NotificationCenter.default.addObserver(
+            forName: ImagesListService.didChangeNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            self?.updateTableViewAnimated()
         }
+    }
+    
+    private func updateTableViewAnimated() {
+        let oldCount = photos.count
+        photos = imagesListService.photos
+        let newCount = photos.count
+        
+        let indexPaths = (oldCount..<newCount).map { IndexPath(row: $0, section: 0) }
+        tableView.insertRows(at: indexPaths, with: .automatic)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -68,6 +70,7 @@ class ImagesListViewController: UIViewController {
 }
 
 // MARK: - UITableViewDelegate
+
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
@@ -85,6 +88,7 @@ extension ImagesListViewController: UITableViewDelegate {
 }
 
 // MARK: - UITableViewDataSource
+
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return photos.count
@@ -99,17 +103,30 @@ extension ImagesListViewController: UITableViewDataSource {
         configCell(for: imageListCell, with: indexPath)
         return imageListCell
     }
-}
-
-// MARK: - Private Methods
-private extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+    
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
         let photo = photos[indexPath.row]
-        cell.cellImage.kf.setImage(with: URL(string: photo.thumbImageURL))
+        cell.cellImage.kf.indicatorType = .activity
+        cell.cellImage.kf.setImage(with: URL(string: photo.thumbImageURL), placeholder: UIImage(named: "placeholder"), options: nil)
         cell.dateLabel.text = DateFormatter.localizedString(from: photo.createdAt ?? Date(), dateStyle: .long, timeStyle: .none)
         
         let isLiked = photo.isLiked
         let likeImage = isLiked ? UIImage(named: "like_button_on") : UIImage(named: "like_button_off")
         cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}
+
+// MARK: - ImagesListCell
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        cellImage.kf.cancelDownloadTask()
     }
 }
